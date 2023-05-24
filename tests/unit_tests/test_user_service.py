ee4248a9dbf3b1a4fc2408474131bab3bd26043e
Elvis Dukaj <edukaj@qti.qualcomm.com>
2023-05-24 22:42:55 +0200

    tests: added unit test to catch exception

--- tests/unit_tests/test_user_service.py
+++ tests/unit_tests/test_user_service.py
@@ -1,4 +1,5 @@
 import pytest
+from app.exceptions import UserNotFoundError
 
 
 @pytest.mark.asyncio
@@ -6,3 +7,10 @@ async def test_user_delete_works_properly(user_service, valid_user_id):
     await user_service.delete_user(valid_user_id)
     assert valid_user_id not in user_service.users_content
     assert valid_user_id not in user_service.profiles_info
+
+
+@pytest.mark.asyncio
+async def test_delete_invalid_user_raise_proper_exception(user_service, invalid_user_delete_id):
+    with pytest.raises(UserNotFoundError):
+        await user_service.delete_user(invalid_user_delete_id)
+        # don't put code here the above line is raising an exception
